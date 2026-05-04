d570750194bad4e25046f50ff43a203be64b74dd
ayushmanmishra2027 <AYUSHMANMISHRA09@GMAIL.COM>
2026-05-04 23:46:19 +0530

    Update music_data.py

---
+++ music_data.py
@@ -0,0 +1,42 @@
+# music_data.py
+print("✅ music_data LOADED")
+MUSIC_DATABASE = {
+    "sadness": [
+        {"song": "Someone Like You", "artist": "Adele", "year": 2011},
+        {"song": "Fix You", "artist": "Coldplay", "year": 2005}
+    ],
+    "joy": [
+        {"song": "Happy", "artist": "Pharrell Williams", "year": 2013},
+        {"song": "Don't Stop Believin'", "artist": "Journey", "year": 1981}
+    ],
+    "love": [
+        {"song": "Perfect", "artist": "Ed Sheeran", "year": 2017},
+        {"song": "All of Me", "artist": "John Legend", "year": 2013}
+    ],
+    "anger": [
+        {"song": "In the End", "artist": "Linkin Park", "year": 2000},
+        {"song": "Break Stuff", "artist": "Limp Bizkit", "year": 1999}
+    ],
+    "fear": [
+        {"song": "Thriller", "artist": "Michael Jackson", "year": 1982},
+        {"song": "Disturbia", "artist": "Rihanna", "year": 2008}
+    ],
+    "surprise": [
+        {"song": "Oops!... I Did It Again", "artist": "Britney Spears", "year": 2000},
+        {"song": "We Found Love", "artist": "Rihanna", "year": 2011}
+    ],
+    "neutral": [
+        {"song": "Imagine", "artist": "John Lennon", "year": 1971},
+        {"song": "Bohemian Rhapsody", "artist": "Queen", "year": 1975}
+    ]
+}
+
+MOOD_EMOJIS = {
+    "sadness": "😢",
+    "joy": "😊",
+    "love": "❤️",
+    "anger": "😠",
+    "fear": "😨",
+    "surprise": "😲",
+    "neutral": "😐"
+}
