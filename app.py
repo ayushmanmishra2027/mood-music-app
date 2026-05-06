2bbe1bf8bac7a85e3fbc9da9597e99025d1a9900
ayushmanmishra2027 <AYUSHMANMISHRA09@GMAIL.COM>
2026-05-06 17:23:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
 import streamlit as st
+from textblob import TextBlob
 from music_data import MUSIC_DATABASE, MOOD_EMOJIS
 
-st.title("🎵 Mood-Based Music Recommender")
+st.title("🎵 NLP Mood-Based Music Recommender")
 
-mood = st.selectbox(
-    "Choose your mood:",
-    list(MUSIC_DATABASE.keys())
+user_text = st.text_area(
+    "How are you feeling today?"
 )
 
-if st.button("Recommend Songs"):
+def detect_mood(text):
 
-    st.subheader(f"Songs for {mood.upper()} {MOOD_EMOJIS[mood]}")
+    analysis = TextBlob(text)
 
-    for song in MUSIC_DATABASE[mood]:
-        st.write(
-            f"🎶 {song['song']} by {song['artist']} ({song['year']})"
+    polarity = analysis.sentiment.polarity
+
+    if polarity > 0.4:
+        return "joy"
+
+    elif polarity > 0:
+        return "love"
+
+    elif polarity < -0.4:
+        return "sadness"
+
+    elif polarity < 0:
+        return "anger"
+
+    else:
+        return "neutral"
+
+if st.button("Recommend Music"):
+
+    if user_text.strip() == "":
+        st.warning("Please enter your feelings.")
+    else:
+
+        mood = detect_mood(user_text)
+
+        st.success(
+            f"Detected Mood: {mood.upper()} {MOOD_EMOJIS[mood]}"
         )
+
+        st.subheader("Recommended Songs")
+
+        for song in MUSIC_DATABASE[mood]:
+
+            st.write(
+                f"🎶 {song['song']} by {song['artist']} ({song['year']})"
+            )
