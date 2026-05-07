ef2e3e2e56017eac33b6ff4d89e50b0abc40ad1f
ayushmanmishra2027 <AYUSHMANMISHRA09@GMAIL.COM>
2026-05-07 12:15:04 +0530

    Update music_data.py

--- music_data.py
+++ music_data.py
@@ -1,32 +1,79 @@
-# music_data.py
 MUSIC_DATABASE = {
     "sadness": [
         {"song": "Someone Like You", "artist": "Adele", "year": 2011},
-        {"song": "Fix You", "artist": "Coldplay", "year": 2005}
+        {"song": "Fix You", "artist": "Coldplay", "year": 2005},
+        {"song": "Let Her Go", "artist": "Passenger", "year": 2012},
+        {"song": "Before You Go", "artist": "Lewis Capaldi", "year": 2019},
+        {"song": "Happier", "artist": "Marshmello ft. Bastille", "year": 2018},
+        {"song": "When I Was Your Man", "artist": "Bruno Mars", "year": 2012},
+        {"song": "Stay With Me", "artist": "Sam Smith", "year": 2014},
+        {"song": "All I Want", "artist": "Kodaline", "year": 2013}
     ],
+
     "joy": [
         {"song": "Happy", "artist": "Pharrell Williams", "year": 2013},
-        {"song": "Don't Stop Believin'", "artist": "Journey", "year": 1981}
+        {"song": "Don't Stop Believin'", "artist": "Journey", "year": 1981},
+        {"song": "Can’t Stop the Feeling!", "artist": "Justin Timberlake", "year": 2016},
+        {"song": "On Top of the World", "artist": "Imagine Dragons", "year": 2012},
+        {"song": "Good Time", "artist": "Owl City & Carly Rae Jepsen", "year": 2012},
+        {"song": "Walking on Sunshine", "artist": "Katrina & The Waves", "year": 1983},
+        {"song": "Best Day of My Life", "artist": "American Authors", "year": 2013},
+        {"song": "Uptown Funk", "artist": "Mark Ronson ft. Bruno Mars", "year": 2014}
     ],
+
     "love": [
         {"song": "Perfect", "artist": "Ed Sheeran", "year": 2017},
-        {"song": "All of Me", "artist": "John Legend", "year": 2013}
+        {"song": "All of Me", "artist": "John Legend", "year": 2013},
+        {"song": "Until I Found You", "artist": "Stephen Sanchez", "year": 2021},
+        {"song": "Just the Way You Are", "artist": "Bruno Mars", "year": 2010},
+        {"song": "Love Story", "artist": "Taylor Swift", "year": 2008},
+        {"song": "Thinking Out Loud", "artist": "Ed Sheeran", "year": 2014},
+        {"song": "Photograph", "artist": "Ed Sheeran", "year": 2014},
+        {"song": "I Don’t Want to Miss a Thing", "artist": "Aerosmith", "year": 1998}
     ],
+
     "anger": [
         {"song": "In the End", "artist": "Linkin Park", "year": 2000},
-        {"song": "Break Stuff", "artist": "Limp Bizkit", "year": 1999}
+        {"song": "Break Stuff", "artist": "Limp Bizkit", "year": 1999},
+        {"song": "Numb", "artist": "Linkin Park", "year": 2003},
+        {"song": "Stronger", "artist": "Kanye West", "year": 2007},
+        {"song": "Believer", "artist": "Imagine Dragons", "year": 2017},
+        {"song": "Enemy", "artist": "Imagine Dragons", "year": 2021},
+        {"song": "Lose Yourself", "artist": "Eminem", "year": 2002},
+        {"song": "Smells Like Teen Spirit", "artist": "Nirvana", "year": 1991}
     ],
+
     "fear": [
         {"song": "Thriller", "artist": "Michael Jackson", "year": 1982},
-        {"song": "Disturbia", "artist": "Rihanna", "year": 2008}
+        {"song": "Disturbia", "artist": "Rihanna", "year": 2008},
+        {"song": "Radioactive", "artist": "Imagine Dragons", "year": 2012},
+        {"song": "Everybody Wants To Rule The World", "artist": "Tears for Fears", "year": 1985},
+        {"song": "Demons", "artist": "Imagine Dragons", "year": 2013},
+        {"song": "bury a friend", "artist": "Billie Eilish", "year": 2019},
+        {"song": "Bad Guy", "artist": "Billie Eilish", "year": 2019},
+        {"song": "Heathens", "artist": "Twenty One Pilots", "year": 2016}
     ],
+
     "surprise": [
         {"song": "Oops!... I Did It Again", "artist": "Britney Spears", "year": 2000},
-        {"song": "We Found Love", "artist": "Rihanna", "year": 2011}
+        {"song": "We Found Love", "artist": "Rihanna", "year": 2011},
+        {"song": "Blinding Lights", "artist": "The Weeknd", "year": 2019},
+        {"song": "Shake It Off", "artist": "Taylor Swift", "year": 2014},
+        {"song": "Levitating", "artist": "Dua Lipa", "year": 2020},
+        {"song": "Cheap Thrills", "artist": "Sia", "year": 2016},
+        {"song": "Dynamite", "artist": "BTS", "year": 2020},
+        {"song": "Wake Me Up", "artist": "Avicii", "year": 2013}
     ],
+
     "neutral": [
         {"song": "Imagine", "artist": "John Lennon", "year": 1971},
-        {"song": "Bohemian Rhapsody", "artist": "Queen", "year": 1975}
+        {"song": "Bohemian Rhapsody", "artist": "Queen", "year": 1975},
+        {"song": "Counting Stars", "artist": "OneRepublic", "year": 2013},
+        {"song": "Viva La Vida", "artist": "Coldplay", "year": 2008},
+        {"song": "Yellow", "artist": "Coldplay", "year": 2000},
+        {"song": "Shape of You", "artist": "Ed Sheeran", "year": 2017},
+        {"song": "Perfect Strangers", "artist": "Jonas Blue", "year": 2016},
+        {"song": "Closer", "artist": "The Chainsmokers", "year": 2016}
     ]
 }
 
